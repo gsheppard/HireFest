c3667960fe2301a6138076dca1cc5e70ffb18141
Gregory Sheppard <gregory.sheppard@live.com>
2014-07-01 14:44:24 -0700

    Fixed routing issue with the Javascript refresher

--- app/assets/javascripts/refresh.js
+++ app/assets/javascripts/refresh.js
@@ -4,7 +4,21 @@ $(function(){
     $.ajax('/refresh', {
       success: function(data){
         console.log('Success');
-        console.log('Data: ' + data.data)
+        console.log('Data: ' + data.data);
+      },
+      error: function(data, err){
+        console.log('Error: ' + err);
+      },
+      dataType: 'json',
+      data: { 'post_id' : $('#refresh_post').val() }
+    });
+  });
+
+  $('#seekers_ajax_btn').on('click', function(){
+    $.ajax('/refresh_seekers', {
+      success: function(data){
+        console.log('Success');
+        console.log('Data: ' + data.data);
       },
       error: function(data, err){
         console.log('Error: ' + err);
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'jobs#index'
   get '/refresh', to: 'jobs#refresh'
+
   get '/seekers', to: 'seekers#index'
+  get '/refresh_seekers', to: 'seekers#refresh'
 end
